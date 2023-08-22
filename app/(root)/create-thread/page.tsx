83fc846319ff24f92d2a65631325cf42d77452d1
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-22 21:42:22 +0530

    Started working with the Create-thread page

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,3 +1,4 @@
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -7,8 +8,16 @@ async function Page() {
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  
-  return <h1 className="head-text">Create Thread</h1>;
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
+    </>
+  );
 }
 
 export default Page;
