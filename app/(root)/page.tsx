7b41b7a302aeabf6fa07ab773d78574b7629fcb5
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-23 20:10:52 +0530

    Started

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
 //app/page.tsx
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+
+  console.log(result);
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+{result.posts.length ===0?(
+  <p className="no-result">No Threads Found !!</p>
+):(
+  <>
+  {result.posts.map((post) =>(
+    <ThreadCard
+    key={post._id}
+    />
+  ))}
+  </>
+)}
+      </section>
+
     </>
   );
 }
