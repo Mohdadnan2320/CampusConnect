082a5f8df1d64500dc83be09acc6c92cd9fe7760
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-25 21:59:51 +0530

    Comment Form

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -30,6 +31,11 @@ const Page = async ({ params }: { params: { id: string } }) => {
                     comments={thread.children}
                 />
             </div>
+            <div className="mt-7">
+                <Comment />
+            </div>
         </section >
     )
 }
+
+export default Page;
