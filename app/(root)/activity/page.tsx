0348be8d44fa47b4d22bffc683921172bdfa973d
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-28 00:16:18 +0530

    Ended for 27 august

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,15 @@
-const Page = async () => {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
     return (
       <section>
         <h1 className="head-text mb-10">Activity</h1>
