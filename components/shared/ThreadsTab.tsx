4b1ccafe418f70956ea1c9572499fabfe9f6808e
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-27 18:34:21 +0530

    ThreadTabs

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  //  TODO: Fetch Profile Threads
+
+    return <section>ThreadTabs</section>;
+};
+
+export default ThreadsTab;
