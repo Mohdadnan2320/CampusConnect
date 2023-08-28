1eedfa7cf3e5d3603a6d2b89ea38f850d9c81944
Manish Kumar Ray <manishray2233@gmail.com>
2023-08-28 23:06:53 +0530

    Done for 28 august

---
+++ app/(root)/communities/[id]/page.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
+import { communityTabs } from "@/constants";
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { fetchCommunityDetails } from "@/lib/actions/community.actions";
+
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const communityDetails= await fetchCommunityDetails(params.id)
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={communityDetails.id}
+        authUserId={user.id}
+        name={communityDetails.name}
+        username={communityDetails.username}
+        imgUrl={communityDetails.image}
+        bio={communityDetails.bio}
+        type="Community"
+      />
+
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
+    </section>
+  );
+}
+
+export default Page;
