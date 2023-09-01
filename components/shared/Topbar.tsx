460a81dd2630c2056c4d782a7fa52ac1763f9b71
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-01 12:33:05 +0530

    Done with components shared

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,16 +1,16 @@
-import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
-import { dark } from "@clerk/themes";
 
 function Topbar() {
-  const isUserLoggedIn = true;
   return (
     <nav className="topbar">
-      <Link href="#" className="flex items-center gap-4">
-        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+
       <div className="flex items-center gap-1">
         <div className="block md:hidden">
           <SignedIn>
@@ -26,6 +26,7 @@ function Topbar() {
             </SignOutButton>
           </SignedIn>
         </div>
+
         <OrganizationSwitcher
           appearance={{
             baseTheme: dark,
